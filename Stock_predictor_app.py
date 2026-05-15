97b0e5f86709fc9d14aed4dd4962fb25a1411455
Arkaprava Bhattacharya <arkaprava181204@gmail.com>
2026-05-15 15:06:35 +0530

    Update Stock_predictor_app.py

--- Stock_predictor_app.py
+++ Stock_predictor_app.py
@@ -5,7 +5,7 @@ from keras.models import load_model
 import streamlit as st
 import matplotlib.pyplot as plt
 
-model = load_model('D:\Projects\Data Analytics\Stock predictor\Stock Predictions Model.keras')
+model = load_model('Stock Predictions Model.keras')
 
 st.header('Stock Predictor App')
 
